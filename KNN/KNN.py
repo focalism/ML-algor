722a97437efbe35216fab94e502d449f44516d24
xzg <xzg@broada.com>
2017-07-12 08:13:23 +0800

    .

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -18,17 +18,57 @@ class KNN():
 
 
     def normalise_data(self,data_set,split_sign):
-        normal_data = {}
+        normal_data = []
         for data in data_set:
             data_info = data.split(split_sign)
-            for i in range(len(data_info)):
-                normal_data[str(i)] = []
-                normal_data[str(i)].append(data_info[i])
-        for i in range(len(normal_data.keys())-1):
-            normal_data[key] = [x/max(normal_data[key]) for x in normal_data[key]]
-        return normal_data
+            normal_data.append([x for x in data_info])
 
-    def 
+        for i in range(len(normal_data)):
+            for j in range(len(normal_data[i])-1):
+                normal_data[i][j] = normal_data[i][j]/max(normal_data[:][j])
+        return normal_data
+        
+
+    def cal_dist(self,sample_data,unkonw_data):
+        result = []
+        dist = [[0]]*len(unkonw_data)
+        dist = [x*len(sample_data) for x in dist]
+        for i  in range(len(unkonw_data)):
+            for j in range(len(unkonw_data)):
+                for k in range(len(unkonw_data[i])):
+                    dist[i][j] += (unkonw_data[i][k]-sample_data[j][k])**2
+                    result[i][j].append([dist[i],sample_data[j][-1]])
+        return result
+
+    def cluster(self,results,k):
+        temp_result = {}
+        for i  in len(results):
+            results[i].sort()
+            if k >len(results[i]):
+                return
+
+            temp = results[i][0:k]
+            for  j in range(len(temp)):
+                if temp[j][1] in temp_result:
+                    temp_result[temp[j][1]] += 1
+                else:
+                    temp_result[temp[j][1]] = 0
+            
+            Attributes[i] = max(temp_result.items(),key = lambda x:x[1])[1]
+
+        return Attributes
+
+            
+
+        
+
+
+        
+
+                
+                
+
+
 
 
 
